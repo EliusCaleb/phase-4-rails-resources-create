24e9e88b7733becb8bf3ca4fb81fda20aa46163e
EliusCaleb <eliuswanyoro@gmail.com>
2022-11-22 03:35:02 +0300

    Learning

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,5 @@
 class BirdsController < ApplicationController
+ 
 
   # GET /birds
   def index
@@ -15,5 +16,15 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+
+   # POST /birds
+   def create
+     #   byebug
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+  end
+
+  
+
 
 end
